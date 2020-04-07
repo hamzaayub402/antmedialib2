a2f10391c7781d9bd5bb94958693d687b3190bab
Egenie Next <egenie.apple@gmail.com>
2020-04-07 14:11:52 +0500

    build gradle and test android method.

--- android/src/main/java/com/reactlibrary/Antmedialib2Module.java
+++ android/src/main/java/com/reactlibrary/Antmedialib2Module.java
@@ -1,5 +1,7 @@
 package com.reactlibrary;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -8,6 +10,7 @@ import com.facebook.react.bridge.Callback;
 public class Antmedialib2Module extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private static final String TAG = "HTAG";
 
     public Antmedialib2Module(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -23,5 +26,10 @@ public class Antmedialib2Module extends ReactContextBaseJavaModule {
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+
+    @ReactMethod
+    public void showLog (){
+        Log.d(TAG, "showLog: Here is Log");
     }
 }
